ed8caa5c6cc9d6899ef0b50133acb8bcf40d03a1
Andy <0andrewmartin0@gmail.com>
2023-11-12 23:17:41 +0000

    Perlin noise landscape

--- Core/Texture.cs
+++ Core/Texture.cs
@@ -1,5 +1,8 @@
 ﻿
 using StbImageSharp;
+using System.Diagnostics;
+using System.Numerics;
+using System.Runtime.InteropServices;
 
 namespace DrawStuff;
 
@@ -8,7 +11,66 @@ public interface GPUTexture {
     int Height { get; }
 }
 
-public record Texture(byte[] Data, int Width, int Height) {
+[StructLayout(LayoutKind.Sequential)]
+public struct PixelRGBA {
+    public byte R;
+    public byte G;
+    public byte B;
+    public byte A;
+}
+
+public record struct Subtexture(Texture Src, int X, int Y, int W, int H) {
+    public static implicit operator Subtexture(Texture t) => new(t, 0, 0, t.Width, t.Height);
+
+    public static implicit operator TCQuad(Subtexture t) =>
+        TCQuad.FromRect(
+            (float)t.X / t.Src.Width, (float)t.Y / t.Src.Height,
+            (float)t.W / t.Src.Width, (float)t.H / t.Src.Height);
+}
+
+public record TCQuad(Vector2 A, Vector2 B, Vector2 C, Vector2 D) {
+    public static TCQuad FromRect(float x, float y, float w, float h) =>
+        new(new(x, y), new(x + w, y), new(x + w, y + h), new(x, y + h));
+}
+
+public class Texture {
+
+    public byte[] Data { get; }
+    public int Width { get; }
+    public int Height { get; }
+
+    public Span<PixelRGBA> Pixels => MemoryMarshal.Cast<byte, PixelRGBA>(Data);
+
+    public Span<PixelRGBA> Row(int row) => Pixels[(row * Width)..][0..Width];
+
+    public Subtexture GetSubtexture(int x, int y, int w, int h) {
+        Debug.Assert(x + w <= Width && y + h <= Height);
+        return new(this, x, y, w, h);
+    }
+
+    public Subtexture Blit(int x, int y, Subtexture tex) {
+        Debug.Assert(x + tex.W <= Width && y + tex.H <= Height);
+        for(int i = 0; i < tex.H; ++i) {
+            var src = tex.Src.Row(tex.Y + i)[tex.X..][0..tex.W];
+            var dest = Row(y + i)[x..];
+            src.CopyTo(dest);
+        }
+        return GetSubtexture(x, y, tex.W, tex.H);
+    }
+
+    public Texture(byte[] data, int width, int height) {
+        Data = data;
+        Width = width;
+        Height = height;
+        Debug.Assert(data.Length == width * height * 4);
+    }
+
+    public Texture(int width, int height) {
+        Data = new byte[width * height * 4];
+        Width = width;
+        Height = height;
+    }
+
     public static Texture Load(string path) {
         var image = ImageResult.FromMemory(
             File.ReadAllBytes(path), ColorComponents.RedGreenBlueAlpha);
--- Samples/CubeWorld/CubeWorld.cs
+++ Samples/CubeWorld/CubeWorld.cs
@@ -4,6 +4,7 @@ using static DrawStuff.ShaderLanguage;
 using System.Numerics;
 
 using CubeVert = CubeShader.VertexData;
+using DotnetNoise;
 
 // Create a window
 var window = Window.Create(WindowOptions.Default with {
@@ -17,16 +18,17 @@ void OnWindowLoad() {
     var ds = IDrawStuff.StartDrawing(window);
     var shader = ds.LoadShader(CubeShader.Config);
 
-    // Load a grass texture
-    var tex = ds.LoadGPUTexture("../../../grass_top.png");
+    // Load a texture atlas
+    var atlas = CubeAtlas.Load(ds);
 
     // Generate some cube chunks
-    Random rand = new();
+    var cubes = CubeModels.Create(atlas);
+    var noise = new FastNoise(1342) { UsedNoiseType = FastNoise.NoiseType.Perlin };
     var world = new Geometry<CubeVert>();
     int chunkSize = 16;
     for(int x = -5; x < 5; ++x)
         for (int z = -5; z < 5; ++z)
-            world.GenerateChunk(rand, new(x * 16, 0, z * 16));
+            world.GenerateChunk(cubes, noise, new(x * 16, 0, z * 16));
 
     // Load to GPU
     var gpuWorld = shader.LoadGeometry(world);
@@ -58,10 +60,10 @@ void OnWindowLoad() {
                 Quaternion.CreateFromYawPitchRoll(time, 0, 0))
             * Matrix4x4.CreateFromQuaternion(
                 Quaternion.CreateFromYawPitchRoll(0, -0.5f, 0))
-            * Matrix4x4.CreateScale(0.05f);
+            * Matrix4x4.CreateScale(0.1f);
 
         // Draw the cubes
-        shader.Draw(gpuWorld, new(transform, new(-1, -1, -1), tex));
+        shader.Draw(gpuWorld, new(transform, new(-1, -1, -1), atlas.Tex));
 
         var textTransform =
             Matrix4x4.CreateTranslation(new Vector3(
@@ -79,6 +81,7 @@ void OnWindowLoad() {
 window.Load += OnWindowLoad;
 window.Run();
 
+// The shader used to draw the cube landscape
 [ShaderProgram]
 partial class CubeShader {
     Mat4 transform;
@@ -94,34 +97,58 @@ partial class CubeShader {
 
     public RGBA Fragment(FragInput v) {
         var cosAngle = dot(normalize(v.Normal), normalize(lightDir));
-        float facingRatio = max(0, cosAngle) * 0.5f;
-        float ambient = (1f - (0.4f + v.Pos.z * 5)) * 0.5f;
+        float facingLight = max(0, cosAngle) * 0.5f;
+        float distanceLight = (1f - (0.4f + v.Pos.z * 5)) * 0.25f;
+        float ambientLight = 0.25f;
         var colour = sample(tex, v.Tc);
-        return vec4(colour.rgb * (facingRatio + ambient), 1);
+        return vec4(colour.rgb * (facingLight + distanceLight + ambientLight), 1);
+    }
+}
+
+// Cube face texture atlas
+public record CubeAtlas(GPUTexture Tex, TCQuad GrassTop, TCQuad GrassSide, TCQuad Dirt) {
+    public static CubeAtlas Load(IDrawStuff ds) {
+        var atlas = ds.LoadTexture("../../../Textures-16.png");
+        return new(ds.LoadGPUTexture(atlas),
+            atlas.GetSubtexture(16 * 3, 16 * 16, 16, 16),
+            atlas.GetSubtexture(16 * 3, 16 * 29, 16, 16),
+            atlas.GetSubtexture(16 * 3, 16 * 30, 16, 16));
+    }
+}
+
+// Models generated for each cube type
+public record CubeModels(Geometry<CubeVert> Grass, Geometry<CubeVert> Dirt) {
+    public static CubeModels Create(CubeAtlas atlas) {
+        var grass = new Geometry<CubeVert>().AddCube(atlas.GrassTop, atlas.GrassSide, atlas.Dirt);
+        var dirt = new Geometry<CubeVert>().AddCube(atlas.Dirt, atlas.Dirt, atlas.Dirt);
+        return new(grass, dirt);
     }
 }
 
 public static class GeometryExtensions {
 
-    public static void AddFace(
+
+    static void AddCubeFace(
         this Geometry<CubeVert> g,
-        Vector3 a, Vector3 b, Vector3 c, Vector3 d) 
+        Vector3 a, Vector3 b, Vector3 c, Vector3 d, TCQuad tp)
     {
         var norm = Vector3.Cross(b - a, c - a);
         g.AddQuad(
-            new (a, norm, new(0, 0)),
-            new (b, norm, new(0, 1)),
-            new (c, norm, new(1, 1)),
-            new (d, norm, new(1, 0)));
+            new (a, norm, tp.A),
+            new (b, norm, tp.B),
+            new (c, norm, tp.C),
+            new (d, norm, tp.D));
     }
 
-    public static Geometry<CubeVert> AddCube(this Geometry<CubeVert> g) {
-        g.AddFace(new(0, 0, 1), new(0, 1, 1), new(1, 1, 1), new(1, 0, 1));
-        g.AddFace(new(1, 0, 0), new(1, 1, 0), new(0, 1, 0), new(0, 0, 0));
-        g.AddFace(new(0, 0, 0), new(0, 0, 1), new(1, 0, 1), new(1, 0, 0));
-        g.AddFace(new(1, 1, 0), new(1, 1, 1), new(0, 1, 1), new(0, 1, 0));
-        g.AddFace(new(1, 0, 0), new(1, 0, 1), new(1, 1, 1), new(1, 1, 0));
-        g.AddFace(new(0, 1, 0), new(0, 1, 1), new(0, 0, 1), new(0, 0, 0));
+    public static Geometry<CubeVert> AddCube(
+        this Geometry<CubeVert> g, TCQuad top, TCQuad side, TCQuad bottom)
+    {
+        g.AddCubeFace(new(1, 1, 0), new(1, 1, 1), new(0, 1, 1), new(0, 1, 0), top);
+        g.AddCubeFace(new(0, 0, 0), new(0, 0, 1), new(1, 0, 1), new(1, 0, 0), bottom);
+        g.AddCubeFace(new(0, 1, 1), new(1, 1, 1), new(1, 0, 1), new(0, 0, 1), side);
+        g.AddCubeFace(new(1, 1, 0), new(0, 1, 0), new(0, 0, 0), new(1, 0, 0), side);
+        g.AddCubeFace(new(1, 1, 1), new(1, 1, 0), new(1, 0, 0), new(1, 0, 1), side);
+        g.AddCubeFace(new(0, 1, 0), new(0, 1, 1), new(0, 0, 1), new(0, 0, 0), side);
         return g;
     }
 
@@ -140,18 +167,37 @@ public static class GeometryExtensions {
         return sb;
     }
 
-    private static Geometry<CubeVert> Cube =
-        new Geometry<CubeVert>().AddCube();
+    public static float Noise(FastNoise noise, Vector2 pos) {
+        return (1 + noise.GetNoise(pos.X, pos.Y)) * 0.5f;
+    }
 
-    public static void GenerateChunk(this Geometry<CubeVert> world, Random rand, Vector3 offset) {
+    public static float LandscapeNoise(FastNoise n, float x, float y) {
+        var pos = new Vector2(x, y);
+        var ridges = Noise(n, (Vector2.One * 10000f) - (pos * 5));
+        ridges = (int)(ridges * 4);
+        ridges /= 4f;
+        var detail = Noise(n, pos * 10);
+        return ridges * 0.5f + detail * 0.5f;
+    }
+
+    public static bool IsGround(FastNoise noise, Vector3 pos) =>
+        pos.Y <= LandscapeNoise(noise, pos.X, pos.Z) * 17;
+
+    public static void GenerateChunk(
+        this Geometry<CubeVert> world,
+        CubeModels cubes,
+        FastNoise noise, Vector3 offset)
+    {
         int chunkSize = 16;
-
         for (int x = 0; x < chunkSize; ++x) {
             for (int y = 0; y < chunkSize; ++y) {
                 for (int z = 0; z < chunkSize; ++z) {
-                    if (rand.NextDouble() > 0.66) {
-                        world.Append(Cube, v => {
-                            v.pos = v.pos.Cpu() + new Vector3(x, y, z) + offset;
+                    var pos = new Vector3(x, y, z) + offset;
+                    if (IsGround(noise, pos)) {
+                        var cube = IsGround(noise, pos + Vector3.UnitY)
+                            ? cubes.Dirt : cubes.Grass;
+                        world.Append(cube, v => {
+                            v.pos = v.pos.Cpu() + pos;
                             return v;
                         });
                     }
